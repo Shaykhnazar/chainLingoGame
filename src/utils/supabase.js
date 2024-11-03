b34a822dff41e107876b5f230ce6c3bd81cd033f
Shaykhnazar <shayx3470941@gmail.com>
2024-11-03 15:15:45 +0500

    supabase .env vars fix

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,12 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-// import dotenv from 'dotenv';
 
-// Load environment variables from .env file
-// dotenv.config();
-
-const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY
-
-// console.log(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
